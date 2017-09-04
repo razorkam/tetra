f50d639ff72de187734e57d552c09486767143fd
razorkam <razorkam@gmail.com>
2017-09-04 23:09:33 +0300

    First commit: main plugin file with some stuff
    First commit: main plugin file with some stuff

---
+++ tetra.py
@@ -0,0 +1,18 @@
+import terminatorlib.plugin as plugin
+
+
+
+AVAILABLE = ['Tetra']
+
+class Tetra(plugin.MenuItem):
+
+    config = None
+
+    def __init__(self):
+        pass #plugin constructor
+
+    def callback(self, menuitems, menu, terminal):
+        pass #main plugin function
+
+
+
